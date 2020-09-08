4aacde974eb962ae14c96a0ba53db9e44f6a6ddf
Marc Teinturier <marc.teinturier@codein.fr>
2020-09-08 12:11:38 +0200

    Including Simonwep/pickr in admin UI

--- src/Codein/eZColorPicker/Resources/encore/ez.config.manager.js
+++ src/Codein/eZColorPicker/Resources/encore/ez.config.manager.js
@@ -4,16 +4,16 @@ module.exports = (eZConfig, eZConfigManager) => {
     eZConfigManager.add({
         eZConfig,
         entryName: 'ezplatform-admin-ui-content-edit-parts-js',
-        newItems: [path.resolve(__dirname, '../public/js/codeincolor.js')],
+        newItems: [path.resolve(__dirname, '../public/content-edit.js')],
     });
     eZConfigManager.add({
         eZConfig,
         entryName: 'ezplatform-admin-ui-content-edit-parts-css',
-        newItems: [path.resolve(__dirname, '../public/scss/edit-parts.scss')],
+        newItems: [path.resolve(__dirname, '../public/content-edit.js')],
     });
     eZConfigManager.add({
         eZConfig,
         entryName: 'ezplatform-admin-ui-location-view-css',
-        newItems: [path.resolve(__dirname, '../public/scss/location-view.scss')],
+        newItems: [path.resolve(__dirname, '../public/location-view.js')],
     });
 };
---
+++ src/Codein/eZColorPicker/Resources/public/location-view.js
@@ -0,0 +1,1 @@
+import './scss/location-view.scss'
