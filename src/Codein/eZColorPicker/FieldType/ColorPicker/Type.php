6bf4571c12d37b9d51ba3521f6d638bc2b8d19b6
Marc Teinturier <marc.teinturier@codein.fr>
2020-09-08 16:46:32 +0200

    Adding FieldDefinition settings

--- src/Codein/eZColorPicker/FieldType/ColorPicker/Type.php
+++ src/Codein/eZColorPicker/FieldType/ColorPicker/Type.php
@@ -3,17 +3,30 @@
 
 namespace Codein\eZColorPicker\FieldType\ColorPicker;
 
+use Codein\eZColorPicker\Form\Type\ColorPickerSettingsType;
 use Codein\eZColorPicker\Form\Type\ColorPickerType;
 use eZ\Publish\SPI\FieldType\Generic\Type as GenericType;
+use EzSystems\EzPlatformAdminUi\FieldType\FieldDefinitionFormMapperInterface;
+use EzSystems\EzPlatformAdminUi\Form\Data\FieldDefinitionData;
 use EzSystems\EzPlatformContentForms\Data\Content\FieldData;
 use EzSystems\EzPlatformContentForms\FieldType\FieldValueFormMapperInterface;
 use Symfony\Component\Form\FormInterface;
 
-final class Type extends GenericType implements FieldValueFormMapperInterface
+final class Type extends GenericType implements FieldValueFormMapperInterface, FieldDefinitionFormMapperInterface
 {
     public function getFieldTypeIdentifier(): string
     {
         return 'codeincolor';
+    }
+
+    public function getSettingsSchema(): array
+    {
+        return [
+            'default' => [
+                'type' => 'string',
+                //'default' => '#00000000',
+            ],
+        ];
     }
 
     public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
@@ -25,4 +38,13 @@ final class Type extends GenericType implements FieldValueFormMapperInterface
             'label' => $definition->getName()
         ]);
     }
+
+    public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data): void
+    {
+        $fieldDefinitionForm->add('fieldSettings', ColorPickerSettingsType::class, [
+            'label' => false,
+        ]);
+    }
+
+
 }
